084bf22b1fe8857a6de3b84ad7a01d6d47bce7f9
liviaalvzs <livs.santana01@gmail.com>
2022-07-12 18:39:04 -0300

    projeto base montado

---
+++ src/products/products.js
@@ -0,0 +1,17 @@
+let productsJson = [
+    {id:1, name:'Dias Perfeitos - Raphael Montes', img:'img/diasperfeitos-raphaelmontes.png', price:20.90},
+    {id:3, name:'Jantar Secreto - Raphael Montes', img:'img/jantarsecreto-raphaelmontes.png', price:20.90},
+    {id:2, name:'Carrie, A Estranha - Stephen King', img:'img/carrieaestranha-stephenking.png', price:20.90},
+    {id:4, name:'It, A Coisa - Stephen King', img:'img/itacoisa-stephenking.png', price:20.90},
+    {id:5, name:'O Iluminado - Stephen King', img:'img/oiluminado-stephenking.png', price:20.90},,
+    {id:6, name:'Duna - Frank Hebert', img:'img/duna-frankhebert.png', price:20.90},
+    {id:7, name:'Messias de Duna - Frank Hebert', img:'img/messiasdeduna-frankhebert.png', price:20.90},
+    {id:8,  name:'Filhos de Duna - Frank Hebert', img:'img/filhosdeduna-frankhebert.png', price:20.90},
+    {id:9,  name:'Eu, Robô - Isaac Asimov', img:'img/eurobo-isaacasimov.png', price:20.90},
+    {id:10, name:'Fundação - Isaac Asimov', img:'img/fundacao-isaacasimov.png', price:20.90},
+    {id:11, name:'Segunda Fundação - Isaac Asimov', img:'img0/segundafundacao-isaacasimov.png', price:20.90},
+    {id:11, name:'Fundação e imperio - Isaac Asimov', img:'img/fundacaoeimpaerio-isaacasimov.png', price:20.90},
+];
+
+export {productsJson};
+
